4e3a1107cd924c70cd7435dc0d9ff0224b453f91
santhosh shanmugam <thosh369@gmail.com>
2024-09-09 14:52:17 +0100

    Formatted output for book report

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ def main():
     path = "books/frankenstein.txt"
     text = get_text(path)
     word_count = get_word_count(text)
-    print(f"Words in text: {word_count}")
     char_count = get_char_count(text)
-    print(f"Characters in text: {char_count}")
+    char_list = convert_dict(char_count)
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document\n")
+    
+    
+    for i in char_list:
+        if i['char'].isalpha():
+                print(f"The '{i['char']}' character was found {i['num']} times")
+
+    print("--- End report ---")
 
 def get_text(path):
     with open(path) as f:
@@ -23,6 +32,11 @@ def get_char_count(text):
             count[i] = 1
     return count 
 
+def convert_dict(char_count):
+    char_list = [{"char": key, "num": value} for key, value in char_count.items()]
+    char_list.sort(reverse=True, key= lambda char_list: char_list["num"])
+    
+    return char_list
 
 if __name__ == "__main__":
     main()
